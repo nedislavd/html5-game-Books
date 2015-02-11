8f2bd4ca9ecb5553132b0f8b155c9c14b9e9cef3
Nedislav Denev <nedislav.denev@gmail.com>
2015-02-11 23:52:19 +0200

    Laying down the groundwork

---
+++ js/book-game.js
@@ -0,0 +1,14 @@
+var gosho = function(){
+    for (var i = 0; i <= 100; i++) {
+        if ( i % 3 == 0 && i % 5 == 0 ) {
+            console.log('fizz buzz ' + i)
+        } else  if ( i % 3 == 0) {
+            console.log('fizz');
+        } else if ( i % 5 == 0) {
+            console.log('buzz');
+        } else {
+            console.log(i);
+        }
+    }
+};
+gosho();
